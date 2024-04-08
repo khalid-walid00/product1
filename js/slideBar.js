d809a7edcfee66cb2fa4d16f536f7f1136b7df23
khalidwalid00 <khalidk50050@gmail.com>
2024-04-08 19:19:47 +0200

    o

---
+++ js/slideBar.js
@@ -0,0 +1,8 @@
+function toggleFunction() {
+    var slidebar = document.getElementById("slidebar");
+    if (slidebar.style.opacity == "0") {
+      slidebar.style.opacity = "1";
+    } else {
+      slidebar.style.opacity = "0";
+    }
+  }
